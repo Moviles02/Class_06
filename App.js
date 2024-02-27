796d6799aa645e4b70ffce370e87de71693f4fdc
edduardaq <andresquiros.ed@gmail.com>
2024-02-26 19:37:04 -0600

    Primer subida al git

---
+++ App.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+		import {View} from 'react-native';
+		import ValiUser from './src/components/fb_Valiusers';
+
+		const App = () => {
+		  return (
+			<View>
+			  <ValiUser />
+			</View>
+		  );
+		};
+
+		export default App;
